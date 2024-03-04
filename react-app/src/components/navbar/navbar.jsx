34fc32abcf8d97ca882aeff9f03ec5395a46feeb
salimxfardeheb <salimfardeheb442@gmail.com>
2024-03-04 18:39:06 +0100

    featuring upper navbar

---
+++ react-app/src/components/navbar/navbar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+import DarkMode from "./DarkMode.jsx"
+import { IoMdSearch } from "react-icons/io";
+import { FaCartShopping } from "react-icons/fa6";
+
+const Navbar = () => {
+  return (
+    <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 z-40">
+      {/* upper Navbar */}
+      <div className="bg-primary/40 py-3 sm:py-0">
+        <div className="container flex justify-between items-center ">
+          <div>
+            <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+              <img src={logo} alt="Logo" className="w-10" />
+              Shopsy
+            </a>
+          </div>
+          {/* search bar */}
+          <div className="flex justify-between items-center gap-4">
+            <div className="relative group hidden sm:block">
+              <input
+                type="text"
+                placeholder="Search"
+                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary"
+              />
+              <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
+            </div>{" "}
+            {/* order button */}
+            <button
+              onClick={() => {
+                alert("Ordering not available yet");
+              }}
+              className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group"
+            >
+              <span className="group-hover:block hidden transition-all duration-200">
+                Order
+              </span>
+              <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
+            </button>
+            {/* Darkmode Switch */}
+            <DarkMode/>
+          </div>
+        </div>
+      </div>
+      {/* lower Navbar */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
